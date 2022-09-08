18521607c55c92b5dd803fef9474de2304572177
KnDjoshua <78736301+KnDjoshua@users.noreply.github.com>
2022-09-08 15:08:31 +0300

    Delete convert dormains to ip.py

--- convert dormains to ip.py
+++
@@ -1,9 +0,0 @@
-import socket
-import time
-print('Hi There,')
-time.sleep(3)
-dormain = input("\033[96mEnter Dormain Name To Convert: ")
-ip = socket.gethostbyname(dormain)
-time.sleep(2)
-print (f"\033[80mIP Address Of {dormain} =>=> {ip}")
-
